90dd6fc6071dae7b63348c3837be070b3f339159
Exzhnik <exzhnik@gmail.com>
2019-05-31 19:26:25 +0400

    delit sampleData

--- lib/pages/stocks.dart
+++ lib/pages/stocks.dart
@@ -7,13 +7,13 @@ import 'package:flutter_candlesticks/flutter_candlesticks.dart';
 
 import 'package:my_shares/setup/previos.dart';
 
-List sampleData = [
-  {"open": 50.0, "high": 100.0, "low": 40.0, "close": 80, "volumeto": 5000.0},
-  {"open": 80.0, "high": 90.0, "low": 55.0, "close": 65, "volumeto": 4000.0},
-  {"open": 65.0, "high": 120.0, "low": 60.0, "close": 90, "volumeto": 7000.0},
-  {"open": 90.0, "high": 95.0, "low": 85.0, "close": 80, "volumeto": 2000.0},
-  {"open": 80.0, "high": 85.0, "low": 40.0, "close": 50, "volumeto": 3000.0},
-];
+// List sampleData = [
+//   {"open": 50.0, "high": 100.0, "low": 40.0, "close": 80, "volumeto": 5000.0},
+//   {"open": 80.0, "high": 90.0, "low": 55.0, "close": 65, "volumeto": 4000.0},
+//   {"open": 65.0, "high": 120.0, "low": 60.0, "close": 90, "volumeto": 7000.0},
+//   {"open": 90.0, "high": 95.0, "low": 85.0, "close": 80, "volumeto": 2000.0},
+//   {"open": 80.0, "high": 85.0, "low": 40.0, "close": 50, "volumeto": 3000.0},
+// ];
 
 class MyApp extends StatelessWidget {
   String text;
@@ -78,13 +78,13 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              OHLCVGraph(
-                  data: sampleData,
-    enableGridLines: true,
-    volumeProp: 0.1,
-    gridLineAmount: 5,
-    gridLineColor: Colors.grey[300],
-    gridLineLabelColor: Colors.grey),
+              // OHLCVGraph(
+    //               data: sampleData,
+    // enableGridLines: true,
+    // volumeProp: 0.1,
+    // gridLineAmount: 5,
+    // gridLineColor: Colors.grey[300],
+    // gridLineLabelColor: Colors.grey),
               ListTile(
                   title: Text(
                     _company?.companyName?.toString() ?? '',
